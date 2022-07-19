9f1a06c8367db551bd0385da4c30c065e749975d
laurasierra17 <laura.sierra17@gmail.com>
2022-07-19 16:29:37 -0400

    complete Letter.js

--- Develop/lib/Letter.js
+++ Develop/lib/Letter.js
@@ -1,5 +1,32 @@
 class Letter {
-  
+  constructor(letter) {
+    this.letter = letter;
+
+    if (letter.match(/[a-z]/i) || letter >= '0' && letter <= '9') {
+        this.visible = false;
+    } else {
+        this.visible = true;
+    }
+  }
+
+  toString = () => {
+    if (this.visible) {
+        return this.letter;
+    }
+    return "_";
+  }
+
+  guess = (otherLetter) => {
+    if (this.letter === otherLetter) {
+        return true
+    }
+    return false;
+  }
+
+  getSolution = () => {
+    return this.letter;
+  }
+
 }
 
 module.exports = Letter;
